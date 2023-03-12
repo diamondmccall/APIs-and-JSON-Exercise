5d3c617117582cc395501a2e9053818aa6d0ad30
Diamond Mccall <singlikediamond@gmail.com>
2023-03-11 18:12:35 -0600

    inital commit

---
+++ KanyeRon/KanyeRon/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using static System.Net.WebRequestMethods;
+
+namespace KanyeRon
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var quote = new Quote(client);
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("------------------------");
+                Console.WriteLine($"Kanye: {quote.Kanye()}");
+
+                Console.WriteLine("------------------------");
+                Console.WriteLine($"Ron: {quote.Ron()}");
+
+            }
+        }
+    }
+}
---
+++ KanyeRon/KanyeRon/Quote.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeRon
+{
+	public class Quote
+	{
+
+        private HttpClient _client;
+
+        public Quote(HttpClient client)
+        {
+            _client = client;
+        }
+
+
+        public string Kanye()
+		{
+            
+
+            var kanyeURL = "https://api.kanye.rest/";
+
+            var kanyeRespone = _client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeRespone).GetValue("quote").ToString();
+
+
+           return kanyeQuote;
+
+            
+        }
+
+        public string Ron()
+        {
+            
+
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronRespone = _client.GetStringAsync(ronURL).Result;
+
+            var ronQuote = JArray.Parse(ronRespone).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+
+            return ronQuote;
+        }
+	}
+}
+
